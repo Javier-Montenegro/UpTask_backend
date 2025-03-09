4c03eef9a7a6232bb54816ae18dae028b5740a86
Javier Montenegro <duke_alex125@hotmail.com>
2025-03-08 22:54:52 -0600

    Solucionando Cors

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -1,17 +1,30 @@
-import { CorsOptions } from "cors";
+import { CorsOptions } from 'cors';
 
 export const corsConfig: CorsOptions = {
-    origin: function (origin, callback) {
-        const whitelist = [process.env.FRONTEND_URL]
 
-        if (process.argv[2] === '--api') {
-            whitelist.push(undefined)
-        }
+  origin: (origin, callback) => {
 
-        if (whitelist.includes(origin)) {
-            callback(null, true)
-        } else {
-            callback(new Error('Error de CORS'))
-        }
+    const whitelist = [process.env.FRONTEND_URL];
+
+
+    if (process.argv[2] === '--api') {
+
+      whitelist.push(undefined, null); // Permitir solicitudes sin origen (Postman, etc.)
+
     }
-}
+
+
+
+    if (!origin || whitelist.includes(origin)) {
+
+      callback(null, true); // Permitir acceso
+
+    } else {
+
+      callback(new Error('Error de CORS: origen no permitido')); // Bloquear acceso
+
+    }
+
+  },
+
+};
