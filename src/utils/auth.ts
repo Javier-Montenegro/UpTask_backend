fd3a34ee13fe03117a93aed5dc3f96c1bde1dbf3
Javier Montenegro <duke_alex125@hotmail.com>
2025-03-08 22:26:54 -0600

    Refactor: Cambiar bcrypt por bcryptjs

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,11 +1,11 @@
-import bcrypt from 'bcrypt'
+import bcryptjs from 'bcryptjs'
 
 
 export const hashPassword = async (password: string) => {
-    const salt = await bcrypt.genSalt(10)
-    return await bcrypt.hash(password, salt)
+    const salt = await bcryptjs.genSalt(10)
+    return await bcryptjs.hash(password, salt)
 }
 
 export const checkPassword = async (enteredPassword: string, storedHash: string) => {
-    return await bcrypt.compare(enteredPassword, storedHash)
+    return await bcryptjs.compare(enteredPassword, storedHash)
 }
